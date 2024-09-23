5d3de7ab9282a06dbd0c1b7f496a0d290ac5fe59
Naveen Prajapati <prajapatinaveen7003@gmail.com>
2024-09-23 15:43:32 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 const app = express();
 const indexRouter = require("./routes/indexroutes");
 
+app.use(cors({
+    origin:true,
+    credentials:true
+}))
 app.use("/api/v1", indexRouter);
 
 module.exports = app
